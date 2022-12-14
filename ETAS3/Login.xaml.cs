17da74f0f1d32173b9d80345f86cc3eaffba7ee5
master <master@SMLAB408PC04_23>
2022-12-13 19:27:03 -0500

    Agregar archivos de proyecto.

---
+++ ETAS3/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ETAS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+            string usuario = "Edison";
+            string contraseña = "12345";
+
+            if(usuario == txtUsuario.Text && contraseña == txtpassword.Text)
+            {
+                Navigation.PushAsync(new Principal(usuario));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "Cerrar");
+            }
+
+
+        }
+    }
+}
